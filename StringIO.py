d0d403f64504b6b1d49da6d7ccb292496a03b387
Nekit28thelegend <ignatenkovivan68@gmail.com>
2024-07-24 14:50:54 +0300

    Add files via upload

---
+++ StringIO.py
@@ -0,0 +1,30 @@
+my_dict = {'Nikita': 9876, 'urban': 55455, 'messi': 88888}
+print(my_dict)
+print(my_dict['Nikita'])
+print(my_dict.get('hurry'))
+del my_dict['messi']
+print(my_dict)
+my_dict.update({'bob':765456, 'simple': 6545})
+print(my_dict)
+
+
+
+
+
+
+
+
+my_set = {1, 4, 1, 4, 5, 5, 7, 7, 8}
+print(my_set)
+my_set = {1, 4, 1, 4, 5, 5, 7, 7, 8, (345, 987, True)}
+print(my_set)
+
+
+
+
+
+
+
+
+
+
